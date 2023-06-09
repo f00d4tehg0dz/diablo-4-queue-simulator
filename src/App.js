91b015b836ff8486036c1920ad60c493e1cfdd50
Adrian Chrysanthou <adrianvfx@gmail.com>
2023-06-09 12:38:41 -0400

    initial commit for fun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const videoRef = useRef(null);
+  const [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    videoElement.loop = true;
+  }, []);
+
+  useEffect(() => {
+      const timer = setInterval(() => {
+        setMinutes(minutes => minutes + 1);
+      }, 600); // Change the increment interval as needed. This is currently set to increment every minute.
+
+      return () => {
+        clearInterval(timer);
+      };
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="video-container">
+      <video autoPlay muted ref={videoRef}>
+        <source src={process.env.PUBLIC_URL + 'videos/diablo4-template.mp4'} type="video/mp4"/>
+        Your browser does not support HTML5 video.
+      </video>
+      <div className="overlay-text">
+        <h1>QUEUED FOR LOGIN ~ {minutes} MINUTES <br/> LEFT...</h1>
+      </div>
     </div>
   );
 }
